fb039b3e29058f9e82d8990dd3a7f0c7f4977a83
Nicolas P. Rougier <Nicolas.Rougier@inria.fr>
2024-02-11 19:13:39 +0100

    Better result display

--- monkey-analysis.py
+++ monkey-analysis.py
@@ -1,4 +1,5 @@
 from player import *
+from prettytable import PrettyTable
 
 import pandas as pd
 data = pd.read_csv("./data/data-processed.csv")
@@ -30,35 +31,65 @@ def get_trials(data, subject_id=None, task_id=None):
     return trials, responses
 
 
-def evaluate(subject_id, players, per_trial=True):
-    """
-    Evaluate players on all lotteries and reports accuracy per
-    trial (default) or per_lottery
-    """    
+def evaluate(subject_id, players):
 
-    s = "        "
-    for pname in players.keys():
-        s = s + "%-5s  " % pname
-    print(s)
-    print("----" + "-------"*len(players))
-    
-#    for name, L in lotteries.items():
+    x = PrettyTable(border=False, align="l")
+    x.field_names = ([bold("Lottery")] +
+                     [bold(p.shortname) for p in players])
     for task_id in range(8):
         trials, R0 = get_trials(data, subject_id, task_id)
-        #trials = generate_trials(10_000, L)
-        #R0 = player.play(trials)
-        
-        s = "L%d: " % task_id
-        for p in players.values():
-            R1 = p.play(trials)
-            if per_trial:
-                d = 1 - ((abs(R0-R1).sum())/len(R0))
+        name = bold("L%d" % task_id)
+        R = [p.play(trials) for p in players]
+        R = [1 - ((abs(R0 - R).sum())/len(R0)) for R in R]
+        Rmin, Rmax = np.min(R), np.max(R)
+        for i in range(len(R)):
+            if abs(R[i] - Rmax) < 1e-5:
+                R[i] = bold("%.3f" % R[i])
+            elif abs(R[i] - Rmin) < 1e-3:
+                R[i] = red("%.3f" % R[i])
             else:
-                d = 1 - (abs(R0.sum()-R1.sum())/len(R0))            
-            s = s + "   %.2f" % d
-        print(s)
-    print()
+                R[i] = "%.3f" % R[i]
+        x.add_row([name] + R)
+    print(x)
 
+
+# def evaluate(subject_id, players, per_trial=True):
+#     """
+#     Evaluate players on all lotteries and reports accuracy per
+#     trial (default) or per_lottery
+#     """    
+
+#     s = "        "
+#     for pname in players.keys():
+#         s = s + "%-5s  " % pname
+#     print(s)
+#     print("----" + "-------"*len(players))
+    
+# #    for name, L in lotteries.items():
+#     for task_id in range(8):
+#         trials, R0 = get_trials(data, subject_id, task_id)
+#         #trials = generate_trials(10_000, L)
+#         #R0 = player.play(trials)
+        
+#         if per_trial:
+#            R_players = [1 - ((abs(R0-R).sum())/len(R0))
+#                 for R in [p.play(trials) for p in players.values()]]
+#         else:
+#            R_players = [1 - (abs(R0.sum()-R.sum())/len(R0)) 
+#                 for R in [p.play(trials) for p in players.values()]]                    
+#         s = "L%d: " % task_id
+#         for r in R_players:
+#            if abs(r - np.max(R_players)) < 1e-3:
+#               s = s + color.BOLD + "   %.2f" % r + color.END
+#            else:
+#               s = s + "   %.2f" % r
+
+#         print(s)
+#     print()
+
+
+
+    
 
 for subject_id in subject_ids:
     print()
@@ -70,37 +101,23 @@ for subject_id in subject_ids:
     bias = 0.5 - responses.sum() / len(trials)
     
     print(color.BOLD + color.RED +
-          "Fitting %s (n=%d, bias=%.3f): " % (subject_id, n, bias),
+          "Fitting %s (n=%d, bias=%.3f): " % (subject_id.upper(), n, bias),
           end="", flush=True)
 
-    print(".", end="", flush=True)
-    rd_fit = RandomPlayer.fit(trials, responses)
-    print(".", end="", flush=True)
-    sg_fit = SigmoidPlayer.fit(trials, responses)
-    print(".", end="", flush=True)
-    p1_fit = ProspectPlayerP1.fit(trials, responses)
-    print(".", end="", flush=True)
-    p2_fit = ProspectPlayerP2.fit(trials, responses)
-    print(".", end="", flush=True)
-    ge_fit = ProspectPlayerGE.fit(trials, responses)
-    print(".", end="", flush=True)
-    tk_fit = ProspectPlayerTK.fit(trials, responses)
+    players = []
+    for cls in [RandomPlayer, SigmoidPlayer, ProspectPlayerP1,
+                ProspectPlayerP2, ProspectPlayerGE, ProspectPlayerTK]:
+        print(".", end="", flush=True)
+        players.append(cls.fit(trials, responses))
         
-    players = { " RD " : rd_fit,
-                " SG " : sg_fit,
-                " P1 " : p1_fit,
-                " P2 " : p2_fit,
-                " GE " : ge_fit,
-                " TK " : tk_fit }
-
     print(" done!" + color.END)
     
     # We compare players parameters
-    print(color.BOLD + "Comparison of fitted players" + color.END)
+    #print(color.BOLD + "Comparison of fitted players" + color.END)
     show(players)
     print()
 
     # We evaluate players, including the original one
-    print(color.BOLD + "Evaluation (per trials)" + color.END)
+    # print(color.BOLD + "Evaluation (per trials)" + color.END)
     evaluate(subject_id, players)
 
